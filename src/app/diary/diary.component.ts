0d4c45eca695fc9cb77e728e8281f25a175b5c24
RonaldKiprotich <protich12@gmail.com>
2020-09-25 00:42:16 +0300

    added component consisting of modal popup and having form inside it

--- src/app/diary/diary.component.ts
+++ src/app/diary/diary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Diary } from '../diary';
 
 @Component({
   selector: 'app-diary',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diary.component.css']
 })
 export class DiaryComponent implements OnInit {
-
+diaries: Diary[]=[
+  new Diary(1,'coding','building an application using angular', new Date(2020,6,20)),
+  new Diary(2,'Reading','i will be reading a book called everything is fucked up', new Date(2020,6,20)),
+  new Diary(3,'playing','playing pool games and cards', new Date(2020,6,20))
+]
   constructor() { }
 
   ngOnInit(): void {
